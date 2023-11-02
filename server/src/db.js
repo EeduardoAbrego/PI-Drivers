3ed8db9ba6577fecd68fc1c80661f6564d06c4b3
eduardoabrego11 <eduardoabrego11@gmail.com>
2023-11-02 01:15:17 -0300

    Final.1

--- server/src/db.js
+++ server/src/db.js
@@ -3,13 +3,13 @@ const { Sequelize } = require("sequelize");
 
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const FunctionDriver = require("./models/Driver");
 const FunctionTeam = require("./models/Team");
 const { dir } = require("console");
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/drivers`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false,
     native: false,
