0cba752262f6ceefb05f4b9b5ffc150325f2a128
Eduardo  Abrego <130002207+EeduardoAbrego@users.noreply.github.com>
2024-04-15 18:38:41 -0300

    Update Form.jsx

--- Client/src/Components/Form.jsx
+++ Client/src/Components/Form.jsx
@@ -50,7 +50,7 @@ const Form = () => {
 
   const createDriver = async (driver) => {
     try {
-      await axios.post("https://drivers-pi-backend.onrender.com/drivers", driver);
+      await axios.post("https://pi-drivers-juyt.onrender.com/drivers", driver);
       alert("usuario creado");
     } catch (error) {
       alert(error.message);
