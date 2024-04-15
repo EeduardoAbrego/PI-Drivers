c212a6e1aba8bc2a01729ebe8f9f7f442f08bc26
Eduardo  Abrego <130002207+EeduardoAbrego@users.noreply.github.com>
2024-04-15 18:25:33 -0300

    Update index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react';
-import store, { persistor } from  "./redux/store";
+
+import store from  "./redux/store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,11 +11,11 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+   
       <BrowserRouter>
         <App />
       </BrowserRouter>  
-    </PersistGate>
+  
   </Provider>
 );
 
